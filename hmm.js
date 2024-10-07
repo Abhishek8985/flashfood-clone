a3a72eab504de77c9f77e03d3c5fa3cbacf2fb98
Kotipalli Abhishek <100757882+Abhishek8985@users.noreply.github.com>
2024-10-07 22:26:02 +0530

    Update hmm.js

--- hmm.js
+++ hmm.js
@@ -2,12 +2,20 @@ function dropcontent() {
     var dropContent = document.querySelector('.drop-content');
     var buttonText = document.getElementById('buttonText');
     var computedStyle = window.getComputedStyle(dropContent);
+    var drop_button = document.querySelector('drop');
 
     if (computedStyle.display === 'none') {
         dropContent.style.display = 'block';
         buttonText.innerText = 'see less';
+        buttonText.style.position='absolute'
+        buttonText.style.top +='300px';
+        buttonText.style.left ='7px';
+        drop_button.style.position='absolute';
+        drop_button.style.position.top+='300px';
     } else {
         dropContent.style.display = 'none';
         buttonText.innerText = 'see more';
+        buttonText.style.top ='12px';
+        drop_button.style.position.top='0px';
     }
 }
